a190d92923cd3dea0310892e356b39a369833172
tommie1001 <tommiedegenaar1@gmail.com>
2026-01-13 14:53:19 +0100

    Add files via upload

---
+++ Bibliotheek-Zoetermeer/script.js
@@ -0,0 +1,23 @@
+const track = document.querySelector('.carousel-track');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+const cards = document.querySelectorAll('.card');
+
+let currentIndex = 0;
+const stepSize = 280; // Breedte kaart + gap
+
+if (nextButton && prevButton) {
+    nextButton.onclick = () => {
+        currentIndex = (currentIndex < cards.length - 3) ? currentIndex + 1 : 0;
+        updateCarousel();
+    };
+
+    prevButton.onclick = () => {
+        currentIndex = (currentIndex > 0) ? currentIndex - 1 : cards.length - 3;
+        updateCarousel();
+    };
+}
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${currentIndex * stepSize}px)`;
+}
